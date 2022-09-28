8fb3c1073632ae243aca7ae40b14bdcd92288401
Felipe Pontes dos Santos <pontesfelipe835@gmail.com>
2022-09-28 17:38:20 -0300

    JS

---
+++ logicaProgramacao/Aula Logica llll/js/carros.js
@@ -0,0 +1,29 @@
+//código do carro
+
+let xCarros = [600, 600, 600];
+let yCarros = [40, 96, 150]; 
+let velocidadeCarros = [2, 2.5, 3.2];
+
+function mostraCarro(){
+  image(imagemCarro, xCarros[0], yCarros[0], 50, 40);
+  image(imagemCarro2, xCarros[1], yCarros[1], 50, 40);
+  image(imagemCarro3, xCarros[2], yCarros[2], 50, 40);
+}
+
+function movimentaCarro(){
+  xCarros[0] -= velocidadeCarros[0];
+  xCarros[1] -= velocidadeCarros[1];
+  xCarros[2] -= velocidadeCarros[2];
+}
+
+function voltaPosicaoInicialDoCarro(){
+  if (xCarros[0] < -50){
+    xCarros[0] = 600
+  }
+  if (xCarros[1] < -50){
+    xCarros[1] = 600
+  }
+  if (xCarros[2] < -50){
+    xCarros[2] = 600
+  }
+}
