3eefb8185ac083875a91dd410c973e11a00163fe
felipe.pontes <pontesfelipe835@gmail.com>
2022-11-23 15:23:51 -0300

    C#

---
+++ csharp/ValidacaoCPF/ValidacaoCPF/Program.cs
@@ -0,0 +1,37 @@
+﻿using Caelum.Stella.CSharp.Validation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidacaoCPF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf1 = "86288366757";
+            string cpf2 = "98745366797";
+            string cpf3 = "22222222222";
+
+            ValidarCPF(cpf1);
+            ValidarCPF(cpf2);
+            ValidarCPF(cpf3);
+        }
+
+        private static void ValidarCPF(string cpf)
+        {
+            try
+            {
+                new CPFValidator().AssertValid(cpf);
+                Debug.WriteLine("CPF válido: " + cpf);
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("CPF inválido: " + cpf + " : " + exc.ToString());
+            }
+        }
+    }
+}
