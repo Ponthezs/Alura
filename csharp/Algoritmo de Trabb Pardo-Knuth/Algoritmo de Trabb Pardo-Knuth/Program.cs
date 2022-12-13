9fc23d36ec53c93a6e5e02604cdec06b357d84be
felipe.pontes <pontesfelipe835@gmail.com>
2022-12-13 15:45:21 -0300

    Algoritmo de Trabb Pardo-Knuth

---
+++ csharp/Algoritmo de Trabb Pardo-Knuth/Algoritmo de Trabb Pardo-Knuth/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Linq.Enumerable;
+using static System.Math;
+
+namespace Algoritmo_de_Trabb_Pardo_Knuth
+{
+    class Tpk
+    {
+        static double F(double t) => Sqrt(Abs(t)) + 5 * Pow(t, 3);
+
+        static void Main()
+        {
+            var a = Range(0, 11).Select(_ => double.Parse(Console.ReadLine())).ToArray();
+            foreach (var i in Range(0, a.Length).Reverse())
+            {
+                var y = F(a[i]);
+                Console.WriteLine(y > 400 ? $"{i} TOO LARGE" : $"{i} {y}");
+            }
+        }
+    }
+}
---
+++ csharp/Algoritmo de Trabb Pardo-Knuth/Algoritmo de Trabb Pardo-Knuth/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Algoritmo de Trabb Pardo-Knuth")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Algoritmo de Trabb Pardo-Knuth")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("24e9579b-b87e-4a35-a39e-db6cf653cfd9")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
