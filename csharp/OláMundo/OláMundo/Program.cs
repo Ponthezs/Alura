57a71c95125c08b533981693eb775159806088a0
Felipe Pontes <pontesfelipe835@gmail.com>
2022-12-20 22:07:21 -0300

    C#

---
+++ csharp/OláMundo/OláMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OláMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Este é meu primeiro programa!");
+        }
+    }
+}
