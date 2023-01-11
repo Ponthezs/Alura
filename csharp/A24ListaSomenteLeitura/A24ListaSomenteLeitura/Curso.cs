8f591a8f6657efab83c621afebd8bd7eb7c41f38
felipe.pontes <pontesfelipe835@gmail.com>
2023-01-11 17:35:28 -0300

    C#

---
+++ csharp/A24ListaSomenteLeitura/A24ListaSomenteLeitura/Curso.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A24ListaSomenteLeitura
+{
+    internal class Curso
+    {
+		private IList<Aula> aulas;
+
+		public IList<Aula> Aulas
+		{
+			get { return new ReadOnlyCollection<Aula>(aulas); }
+			
+		}
+
+		private string nome;
+		private string instrutor;
+
+        public Curso(string nome, string instrutor)
+        {
+            this.nome = nome;
+            this.instrutor = instrutor;
+			this.aulas = new List<Aula>();
+        }
+
+        public string Nome
+		{
+			get { return nome; }
+			set { nome = value; }
+		}
+
+
+		public string Instrutor
+		{
+			get { return instrutor; }
+			set { instrutor = value; }
+		}
+
+        internal void Adiciona(Aula aula)
+        {
+            this.aulas.Add(aula);
+        }
+
+		public int TempoTotal
+		{
+			get
+			{
+				//int total = 0;
+				//foreach (var aula in aulas)
+				//{
+				//	total += aula.Tempo;
+				//}
+				//return total;
+
+				//LINQ = LANGUAGE INTEGRATED QUERY
+				//CONSULTA INTEGRADA Á LINGUAGEM  ------>
+
+				return aulas.Sum(aula => aula.Tempo);
+
+
+			}
+		}
+
+        public override string ToString()
+        {
+			return $"Curso: {nome} , Tempo: {TempoTotal} , Aulas: {string.Join(",", aulas)} ";
+        }
+
+
+    }
+}
---
+++ csharp/A24ListaSomenteLeitura/A24ListaSomenteLeitura/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A24ListaSomenteLeitura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Curso csharpcolecoes = new Curso("C# Collections", "Marcelo Oliveira");
+            csharpcolecoes.Adiciona(new Aula("Trabalhando com Listas", 21));
+            Imprimir(csharpcolecoes.Aulas);
+
+            //Adicionar 2 aulas
+            csharpcolecoes.Adiciona(new Aula ("Criando uma Aula", 20));
+            csharpcolecoes.Adiciona(new Aula("Modelando Coleções", 19));
+
+            //Imprimir
+            Imprimir(csharpcolecoes.Aulas);
+
+            //Ordenar a lista de aulas
+            //csharpcolecoes.Aulas.Sort();
+
+            //Copiar a lista para outra lista
+            List<Aula> aulasCopiadas = new List<Aula>(csharpcolecoes.Aulas);
+
+            //Ordenar a cópia
+            aulasCopiadas.Sort();
+            Imprimir(aulasCopiadas);
+
+            //Totaliza o tempo de curso
+            Console.WriteLine(csharpcolecoes.TempoTotal);
+
+            Console.WriteLine(csharpcolecoes);
+
+            //Imprimir detalhes do curso
+
+
+        }
+
+        private static void Imprimir(IList<Aula> aulas)
+        {
+            Console.Clear();
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+        }
+
+    }
+}
