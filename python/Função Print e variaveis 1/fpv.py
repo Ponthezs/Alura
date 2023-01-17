a5c5663123aef11a247c8f67bc1111dc0f4c3408
felipe.pontes <pontesfelipe835@gmail.com>
2023-01-17 15:39:18 -0300

    PY
    python

---
+++ python/Função Print e variaveis 1/fpv.py
@@ -0,0 +1,27 @@
+print ("ola mundo")
+"help"
+
+print(...)
+ #print(value, ..., sep=' ', end='\n', file=sys.stdout, flush=False)
+
+   # Prints the values to a stream, or to sys.stdout by default.
+    #Optional keyword arguments:
+    #file:  a file-like object (stream); defaults to #the current sys.stdout.
+    #sep:   string inserted between values, default a space.
+    #end:   string appended after the last value, default a newline.
+    #flush: whether to forcibly flush the stream.
+
+print ("Brasil","é Hexa","Campeão", "5")
+
+pais = "Argentina"
+
+print(pais + " ganhou copa do mundo em 2022")
+
+idade = 12 + 15;
+
+print("Idade de Jorge é" + idade)
+
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
---
+++ python/Tipagem do Python 2/Tp.py
@@ -0,0 +1,16 @@
+#<class "str">
+#str pais = "Brasil"
+pais = "Brasil"
+pais = 644
+type(pais)
+#<class "int">
+
+#Além de funcionar, o tipo da variável também muda! O tipo da variável mudou dinamicamente, de acordo com o valor que é atribuído a ela, logo, o tipo da variável é definido de acordo com o valor que ela guarda, isso faz parte da tipagem dinâmica do Python.
+
+pais = 5.8
+type(pais)
+#<class "Float">
+
+idade_esposa = 20
+perfil_vip = 'Flávio Almeida'
+recibos_em_atraso = 30
