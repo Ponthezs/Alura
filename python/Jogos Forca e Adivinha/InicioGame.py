4c3c404a5ad8ea5570076df0a031171e705f81b1
felipe.pontes <pontesfelipe835@gmail.com>
2023-01-18 14:50:20 -0300

    PY
    python

---
+++ python/Jogos Forca e Adivinha/InicioGame.py
@@ -0,0 +1,21 @@
+import forca
+import adivinha
+
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Adivinhação")
+
+    jogo = int(input("Qual jogo? "))
+
+    if (jogo == 1):
+        print("Jogando forca")
+        forca.jogar()
+    elif (jogo == 2):
+        print("Jogando adivinhação")
+        adivinha.jogar()
+
+if (__name__ == "__main__"):
+    escolhe_jogo()
---
+++ python/Jogos Forca e Adivinha/forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao jogo da Forca!")
+    print("*********************************")
+
+    print("Game Over")
+    
+if (__name__ == "__main__"):
+    jogar()
