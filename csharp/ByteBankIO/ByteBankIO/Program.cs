5a4ca22cfb6d5646150dd0a530ad72ebdbfca9e4
felipe.pontes <pontesfelipe835@gmail.com>
2023-02-03 10:53:54 -0300

    C#

--- csharp/ByteBankIO/ByteBankIO/Program.cs
+++ csharp/ByteBankIO/ByteBankIO/Program.cs
@@ -7,59 +7,60 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        var enderecoDoArquivo = "contas.txt";
-
-        using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        static void Main(string[] args)
         {
-            var numeroDeBytesLidos = -1;
 
-            var buffer = new byte[1024]; //1KB      
+            var enderecoDoArquivo = "contas.txt";
 
-            while (numeroDeBytesLidos != 0)
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
             {
-                numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-                EscreverBuffer(buffer);
+                var numeroDeBytesLidos = -1;
+
+                var buffer = new byte[1024]; //1KB      
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+
+                    Console.WriteLine($"Bytes lidos: {numeroDeBytesLidos}");
+
+                }
+
+                // Devoluções:
+                //  O número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+                //  bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+                //  zero, se o final do fluxo for atingido
+
+
+                // public override int Read(byte[] array, int offset, int count);
+                fluxoDoArquivo.Close();
+
+                Console.ReadLine();
             }
-
-
-            //var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
-
-            //var buffer = new byte[1024]; //1KB
-
-            //while (numeroDeBytesLidos != 0)
-            //{
-            //    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-            //    EscreverBuffer(buffer);
-            //}
-
-            // Devoluções:
-            // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
-            // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
-            //zero, se o final do fluxo for atingido
-
-            // public override int Read(byte[] array, int offset, int count)
-            // public override int Read(byte[] array, int offset, int count);
-            fluxoDoArquivo.Close();
-
-        Console.ReadLine();
-    }
-
-    static void EscreverBuffer(byte[] buffer)
-    {
-
-        var utf8 = new UTF8Encoding();
-
-        var texto = utf8.GetString(buffer);
-        Console.Write(texto);
-
-        /*
-        foreach (var meuByte in buffer)
-        {
-            Console.Write(meuByte);
-            Console.Write(" ");
         }
-        */
-    }
+
+
+
+        // ...
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+
+            var utf8 = new UTF8Encoding();
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            //public virtual string GetString(byte[] bytes, int index, int count);
+
+            Console.Write(texto);
+
+            /*
+            foreach (var meuByte in buffer)
+            {
+                    Console.Write(meuByte);
+                    Console.Write(" ");
+            }
+            */
+        }
 }
