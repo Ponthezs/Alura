fc2d3f2260633715feb796f4d1777078f50939ff
felipe.pontes <pontesfelipe835@gmail.com>
2023-02-22 09:10:31 -0300

    TypeScript

---
+++ react/alura-book/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import logo from './img/logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
